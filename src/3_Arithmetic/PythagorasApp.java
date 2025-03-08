1c3d8324fb16c1ec2e12906c301170bd1c75ce51
Taiwo <ttejuosho@outlook.com>
2025-03-07 19:57:32 -0600

    Add Pythagoras App

---
+++ src/3_Arithmetic/PythagorasApp.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PythagorasApp{
+    public static void main (String[] args){
+        // Initialize Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Dispose Scanner
+        scanner.close();
+    }
+}
